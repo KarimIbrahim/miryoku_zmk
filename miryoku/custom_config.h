4e003d75046dc33996d25b1b633fd7b8fea6cff7
Karim Ibrahim <38266294+KarimIbrahim@users.noreply.github.com>
2023-10-12 17:26:22 -0700

    Remapping the num layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,3 +6,10 @@
 U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_MOUSE, TAB),U_LT(U_NAV, SPACE),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+
+
+#define MIRYOKU_LAYER_NUM \
+U_NA,         &kp N7,            &kp N8,            &kp N9,            U_NA,         U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+U_NA,         &kp N4,            &kp N5,            &kp N6,            U_NA,         U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+U_NA,         &kp N1,            &kp N2,            &kp N3,            U_NA,         U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              \
+U_NP,         U_NP,              &kp DOT,           &kp N0,            U_NA,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
