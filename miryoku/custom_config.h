294da565a9dc63ce344b91a45301202931069880
Karim Ibrahim <38266294+KarimIbrahim@users.noreply.github.com>
2023-10-14 10:49:59 -0700

    Fixing the right hand modifiers.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,21 +3,21 @@
 
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MT_L(LGUI, A),     U_MT_L(LALT, S),     U_MT_L(LCTRL, D),    U_MT_L(LSHFT, F),    &kp G,             &kp H,             U_MT_R(LSHFT, J),    U_MT_R(LCTRL, K),    U_MT_R(LALT, L),     U_MT_R(LGUI, SQT),   \
+U_MT_L(LGUI, A),     U_MT_L(LALT, S),     U_MT_L(LCTRL, D),    U_MT_L(LSHFT, F),    &kp G,             &kp H,             U_MT_R(RSHFT, J),    U_MT_R(RCTRL, K),    U_MT_R(RALT, L),     U_MT_R(RGUI, SQT),   \
 U_LT(U_BUTTON, Z), U_MT_L(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT_R(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, TAB),U_LT(U_SYM, SPACE),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 
 #define MIRYOKU_LAYER_EXTRA \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
-U_MT_L(LGUI, A),     U_MT_L(LALT, R),     U_MT_L(LCTRL, S),    U_MT_L(LSHFT, T),    &kp G,             &kp M,             U_MT_R(LSHFT, N),    U_MT_R(LCTRL, E),    U_MT_R(LALT, I),     U_MT_R(LGUI, O),     \
+U_MT_L(LGUI, A),     U_MT_L(LALT, R),     U_MT_L(LCTRL, S),    U_MT_L(LSHFT, T),    &kp G,             &kp M,             U_MT_R(RSHFT, N),    U_MT_R(RCTRL, E),    U_MT_R(RALT, I),     U_MT_R(RGUI, O),     \
 U_LT(U_BUTTON, Z), U_MT_L(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT_R(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, TAB),U_LT(U_SYM, SPACE),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 
 #define MIRYOKU_LAYER_NUM \
 U_NA,         &kp N7,            &kp N8,            &kp N9,            U_NA,         U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
-U_NA,         &kp N4,            &kp N5,            &kp N6,            U_NA,         U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+U_NA,         &kp N4,            &kp N5,            &kp N6,            U_NA,         U_NA,              &kp RSHFT,         &kp RCTRL,         &kp RALT,          &kp RGUI,          \
 U_NA,         &kp N1,            &kp N2,            &kp N3,            U_NA,         U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              \
 U_NP,         U_NP,              U_NA,              &kp DOT,           &kp N0,       U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
