66cdebd56c1b3d57702fc0d52b2f359c223370a5
Karim Ibrahim <karimibr@amazon.com>
2024-08-23 03:43:21 -0700

    Fixing the slash typo.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -9,7 +9,7 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp ESC           K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp BSLSH \
+&kp ESC           K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp BSLH \
 U_LT(U_MEDIA, TAB)  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp SQT \
 &kp LSHFT           K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp RSHFT \
                               K32  K33  K34       K35  K36  K37
@@ -48,21 +48,17 @@ U_NA,       &kp N1,  &kp N2,     &kp N3,    &kp N0,     U_NA,  U_NA,          U_
 U_NP,       U_NP,    &kp MINUS,  &kp PLUS,  &kp SPACE,  U_NA,  U_NA,          U_NA,           U_NP,         U_NP
 
 
-#define MIRYOKU_LAYER_SYM \
-&kp GRAVE,  &kp AT,              &kp UNDER,             &kp PRCNT,  U_NA,      U_NA,        &kp LBRC,  &kp RBRC,             &kp AMPS,            U_NA,  \
-&kp MINUS,  U_MT(LALT, DLLR),  U_MT(LCTRL, ASTRK),  &kp HASH,   U_NA,      U_NA,        &kp LPAR,  U_MT(RCTRL, RPAR),  U_MT(RALT, PIPE),  &kp COLON,  \
-&kp TILDE,  &kp CARET,           U_NA,                  &kp BSLH,   U_NA,      &kp QMARK,   &kp LBKT,  &kp RBKT,             &kp EXCL,            U_NA,  \
-U_NP,       U_NP,                U_NA,                  U_NA,       U_NA,      U_NA,   U_NA,      U_NA,                 U_NP,                U_NP
-
-
 // #define MIRYOKU_LAYER_SYM \
-// &kp EXCL,   &kp AT,            &kp HASH,            &kp DLLR,   &kp PRCNT,  &kp CARET,  &kp AMPS,  &kp ASTRK,          &kp LPAR,          &kp RPAR,      \
-// &kp MINUS,  U_MT(LALT, PLUS),  U_MT(LCTRL, UNDER),  &kp EQUAL,  U_NA,       U_NA,       U_NA,      U_MT(RCTRL, LBRC),  U_MT(RALT, LBRC),  &kp RBRC,  \
-// &kp GRAVE,  U_NA,              U_NA,                &kp TILDE,  U_NA,       U_NA,       U_NA,      U_NA,               &kp LBKT,          &kp RBKT,  \
-// U_NP,       U_NP,              U_NA,                U_NA,       U_NA,       U_NA,       U_NA,      U_NA,               U_NP,              U_NP
-//
-//
-//
+// &kp GRAVE,  &kp AT,              &kp UNDER,             &kp PRCNT,  U_NA,      U_NA,        &kp LBRC,  &kp RBRC,             &kp AMPS,            U_NA,  \
+// &kp MINUS,  U_MT(LALT, DLLR),  U_MT(LCTRL, ASTRK),  &kp HASH,   U_NA,      U_NA,        &kp LPAR,  U_MT(RCTRL, RPAR),  U_MT(RALT, PIPE),  &kp COLON,  \
+// &kp TILDE,  &kp CARET,           U_NA,                  &kp BSLH,   U_NA,      &kp QMARK,   &kp LBKT,  &kp RBKT,             &kp EXCL,            U_NA,  \
+// U_NP,       U_NP,                U_NA,                  U_NA,       U_NA,      U_NA,   U_NA,      U_NA,                 U_NP,                U_NP
+#define MIRYOKU_LAYER_SYM \
+&kp EXCL,   &kp AT,            &kp HASH,            &kp DLLR,   &kp PRCNT,  &kp CARET,  &kp AMPS,  &kp ASTRK,          &kp LPAR,          &kp RPAR,      \
+&kp MINUS,  U_MT(LALT, PLUS),  U_MT(LCTRL, UNDER),  &kp EQUAL,  U_NA,       U_NA,       U_NA,      U_MT(RCTRL, LBRC),  U_MT(RALT, LBRC),  &kp RBRC,  \
+&kp GRAVE,  U_NA,              U_NA,                &kp TILDE,  U_NA,       U_NA,       U_NA,      U_NA,               &kp LBKT,          &kp RBKT,  \
+U_NP,       U_NP,              U_NA,                U_NA,       U_NA,       U_NA,       U_NA,      U_NA,               U_NP,              U_NP
+
 #define MIRYOKU_LAYER_MEDIA \
 U_BOOT,    &u_to_U_TAP,  &u_to_U_EXTRA,  &u_to_U_BASE,   U_NA,  U_RGB_EFF,    U_RGB_HUI,     U_RGB_SAI,     U_RGB_BRI,    U_RGB_TOG,   \
 &kp LGUI,  &kp LALT,     &kp LCTRL,      &kp LSHFT,      U_NA,  &kp C_PREV,   &kp C_VOL_DN,  &kp C_VOL_UP,  &kp C_NEXT,   U_EP_TOG,    \
